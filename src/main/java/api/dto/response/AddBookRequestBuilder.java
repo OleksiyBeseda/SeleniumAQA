d135cf9ff3ed0e67387b6ee565d6b35c0381c31e
Oleksiy Beseda <beseda.odessa@gmail.com>
2023-10-06 18:18:56 +0300

    hw22

---
+++ src/main/java/api/dto/response/AddBookRequestBuilder.java
@@ -0,0 +1,28 @@
+package api.dto.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+
+
+@Data
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+
+public class Book{
+    public String isbn;
+    public String title;
+    public String subTitle;
+    public String author;
+    public Date publish_date;
+    public String publisher;
+    public int pages;
+    public String description;
+    public String website;
+}
+
+
